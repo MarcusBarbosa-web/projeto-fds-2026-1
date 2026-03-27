20f2c486fe9eab6033d5d4e76b5b72f9368ad9a8
Luiza Costa <cwluiza029@gmail.com>
2026-03-27 20:28:27 -0300

    feat: template historico e ajustes na estrutura de arquivos

--- setup/urls.py
+++ setup/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from monitor.views import index
+from monitor import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='monitor/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('historico/', views.historico, name='historico'),
 ]
