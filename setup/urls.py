c66e423ea09dbd1de3d1f205dc44620a2688c530
Luiza Costa <cwluiza029@gmail.com>
2026-03-26 21:20:23 -0300

    feat: adiciona rota e arquivo de template do login

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from monitor.views import index
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='monitor/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
