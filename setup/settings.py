088625a66c5b9685332cc890a0f4a53288c29799
MarcusBarbosa <mvpb@cesar.school>
2026-03-28 22:49:12 -0300

    mais commit para o deploy

--- setup/settings.py
+++ setup/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-n7m#*sww%sly1=rpqke_@5i0o+^b%ir-)khx757o20#cbiytm^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
