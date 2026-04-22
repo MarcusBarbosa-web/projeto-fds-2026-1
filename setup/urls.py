f66723b6b02632d2675129762d508001709eee20
José Milton <miltoneto4231@gmail.com>
2026-04-22 18:17:58 -0300

    criação de um urls.py para a tela de cadastro

--- setup/urls.py
+++ setup/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
     path('historico/', views.historico, name='historico'),
     path('incidentes-ativos/', views.incidentes_ativos, name='incidentes_ativos'),
     path('registrar-novo/', views.registrar_incidente, name='registrar_novo'),
+    path('cadastro/', views.cadastro_view, name='cadastro'),
 ]
