007cc5cefe471de28458b6a5909ec65f788ed06e
José Milton <miltoneto4231@gmail.com>
2026-04-22 11:36:24 -0300

    Mudança na definição da config para DEBUG = True

--- setup/settings.py
+++ setup/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-n7m#*sww%sly1=rpqke_@5i0o+^b%ir-)khx757o20#cbiytm^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
