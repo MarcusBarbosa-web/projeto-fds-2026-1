dd2e071da8e9a96cbe8e9d954c745890e31d2f15
MarcusBarbosa <mvpb@cesar.school>
2026-04-26 16:51:59 -0300

    Corrige max_length na migration 0003

--- monitor/migrations/0003_remove_incidente_data_inicio_and_more.py
+++ monitor/migrations/0003_remove_incidente_data_inicio_and_more.py
@@ -31,7 +31,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='incidente',
             name='status',
-            field=models.CharField(choices=[('Funcionando', 'Funcionando'), ('Instável', 'Instável'), ('Fora do Ar', 'Fora do Ar')], default=django.utils.timezone.now, max_length=20),
+            field=models.CharField(choices=[('Funcionando', 'Funcionando'), ('Instável', 'Instável'), ('Fora do Ar', 'Fora do Ar')], default=django.utils.timezone.now, max_length=50),
             preserve_default=False,
         ),
         migrations.AlterField(
